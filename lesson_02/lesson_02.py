4ce9d6fab2e2b43f911c80c509c1487a36a96209
OPanchenko <pan.pan.4it@gmail.com>
2023-11-26 21:02:28 +0200

    fix lesson 02

--- lesson_02/lesson_02.py
+++ lesson_02/lesson_02.py
@@ -15,13 +15,13 @@ first_line = """<tr><td><a href='https://link.company.com/screens/drqs{drqs_numb
     {drqs_number}</a></td><td>{routeToGroup}</td><td>{createdDate}</td><td>{owner}</td>
     <td>{status}</td><td>{last_update}</td><td>{author}</td><td>{skipped_tests}</td>
     <td>{scope}</td></tr>"""
-print(first_line)
+# print(first_line)
 
 second_line = ("asdfghjklqwertyu"
     "1234567890"
     ")(*&^%#%#&*&^%??><<@!)"
     "djfhdjd,f")
-print(second_line)
+# print(second_line)
 # Оператори дій
 1+1
 1-2
@@ -53,21 +53,21 @@ more Python"""
 aples = 5
 users = 2
 count_aples = aples / users
-print(count_aples)
+# print(count_aples)
 
 ## Строки
 hello = "hello"
 word = 'word'
 english_words = 'I don\'t know'
-print(english_words)
+# print(english_words)
 ## Кортежі (Tuples)
 (0, 2, 4, 6)
 ## Списки [Lists]
 new_list = ["a", 0, 1.2, hello]
-print(new_list)
+# print(new_list)
 ## Набори {Sets}
 simple_set = { 1, 2, 3, 1}
-print(simple_set)
+# print(simple_set)
 ## Словники {Dictionaries: 2}
 samle_dict = {"key": "value"}
 ## None
@@ -90,31 +90,31 @@ class SampleObj():
         print("YAHHHHHHHHHHOOOOOOOOOOOOOOOOOO")
 yet_other_obj = SampleObj()
 ## Атрибути об’єкта
-print(yet_other_obj.a)
+# print(yet_other_obj.a)
 ## Елементи об’єкта
-print(samle_dict["key"])
+# print(samle_dict["key"])
 ## Методи -  виклик операції з об’єктом
 yet_other_obj.some_def()
 # Числові операції
 ## Ділення
 s = 22 / 7
-print(s)
+# print(s)
 s2 = 22 // 7
-print(s2)
+# print(s2)
 s3 = 22 % 7
-print(s3)
+# print(s3)
 s4 = 22 - (7 * s2)
-print(s4 == s3)
+# print(s4 == s3)
 ## Множення, додавання, віднімання
 x = 2
 x2 = x * 2
-print(x2)
-print("*" * 8)
-print("sa"+"sha")
+# print(x2)
+# print("*" * 8)
+# print("sa"+"sha")
 
 
 ## Піднесення у степінь
-print (2**8)
+# print (2**8)
 ## Порівняння
 (1 < 2) and (2 < 3)
 (2 < 1) or (2 < 3)
@@ -124,24 +124,25 @@ print (2**8)
 year = 2006
 event = 'referendum'
 msg = f'Results of the {year} {event.capitalize()}'
-print(msg)
+# print(msg)
 ## format
 yes_votes = 42_572_654
 no_votes = 43_132_495
 percentage = yes_votes / (yes_votes + no_votes)
-print("{1:10d} YES votes \n{0:10d} NO".format(yes_votes, no_votes))
+# print("{1:10d} YES votes \n{0:10d} NO".format(yes_votes, no_votes))
 
 for x in range(1, 11):
-    print('{0:2d} {1:4d} {2:6d}'.format(x, x*x, x*x*x))
+    pass
+    # print('{0:2d} {1:4d} {2:6d}'.format(x, x*x, x*x*x))
 
 ## concatenate
 f_list = ["aple", "banana", "kivi"]
 fruits = ", ".join(f_list)
-print(fruits)
+# print(fruits)
 
 ## multiply
 
 ## ввод
-eggs = input("Скількі яєць на сніданок бажаєте? ")
-eggs = int(eggs)
-print(f"Ви замовили {eggs*2} яєць (ваш бонус враховано)")
+# eggs = input("Скількі яєць на сніданок бажаєте? ")
+# eggs = int(eggs)
+# print(f"Ви замовили {eggs*2} яєць (ваш бонус враховано)")
